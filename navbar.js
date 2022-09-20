2d72cfbe57a1a8c312bff02b3207cb513e802ea8
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-09-20 19:34:39 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { signIn, signOut } from 'next-auth/react'
+
+function Navbar() {
+    return (
+        <>
+            <Link href='/api/auth/signin'>
+                <a onClick={e => {
+                    e.preventDefault();
+                    signIn();
+                }}>SignIn</a>
+            </Link>
+            <Link href='/api/auth/signout'>
+                <a onClick={e => {
+                    e.preventDefault();
+                    signOut();
+                }}>Sign out</a>
+            </Link>
+        </>
+    )
+}
+export default Navbar;
